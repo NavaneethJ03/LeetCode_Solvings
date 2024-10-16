bc5488ad36d453afdc25fa2dab42d05b71807fd3
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-10-16 23:44:08 +0530

    Update 682. Baseball Game.py

--- 682. Baseball Game.py
+++ 682. Baseball Game.py
@@ -1,3 +1,6 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stk = []
+        for ops in operations:
+            if ops == "+":
+                stk.append(int(stk[-1]) + int(stk[-2]))
