cd4d8497ea8c889e349d96831ba2d386b195abb9
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-10-19 18:27:14 +0530

    Update 83. Remove Duplicates from Sorted List.py

--- 83. Remove Duplicates from Sorted List.py
+++ 83. Remove Duplicates from Sorted List.py
@@ -2,3 +2,5 @@ class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur =  head 
         while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
