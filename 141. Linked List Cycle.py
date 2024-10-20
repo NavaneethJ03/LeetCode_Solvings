afc6b8370a2fd4316259b9621fd2b96cd87d682c
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-10-20 22:56:34 +0530

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -2,3 +2,6 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         dummy = ListNode()
         dummy.next = head
+        fast = slow = dummy
+        
+        while fast and fast.next:
