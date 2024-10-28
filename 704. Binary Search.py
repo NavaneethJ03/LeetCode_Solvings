f28a65f22dc115c70739861e41a3f0f76580c329
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-10-28 19:48:26 +0530

    Update 704. Binary Search.py

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
         l = 0
-        r = n - 1 
+        r = n - 1
         
         while l <= r:
             m  = l + ((r - l)//2)
