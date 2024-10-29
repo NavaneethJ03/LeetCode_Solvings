9da3dd69184b51d6c6eb62a482e1d101d8296003
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-10-29 22:45:03 +0530

    Create 125. Valid Palindrome.py

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_dup=''
+        for i in s.lower():
+            if i.isalnum():
+                s_dup=s_dup+i
+        if s_dup==s_dup[::-1]:
+            return True
+        return False
