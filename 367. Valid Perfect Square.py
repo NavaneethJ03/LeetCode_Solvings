e3df52f901a4c7fc818b056e5105a39c2ebdfbb6
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-12-04 00:00:27 +0530

    Update 367. Valid Perfect Square.py

--- 367. Valid Perfect Square.py
+++ 367. Valid Perfect Square.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        L = 1 
+        R = num 
+        
+        while L <= R :
+            M =  L +((R-L)//2)
+            m_sq = M * M
+            if num ==  m_sq:
+                return True
+            elif num > m_sq:
+                L = M + 1
+            else:
+                R =  M - 1 
+        return False
+        
