ba1e1dd8b095962744bf6f01b6f84edf23a8d2fb
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-12-17 23:50:26 +0530

    Update 100. Same Tree.py

--- 100. Same Tree.py
+++ 100. Same Tree.py
@@ -6,4 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def Same(p , q):
+            if not p and not q: 
+                return True 
+            if (not p and q) or (not q and p):
+                return False 
+            if p.val != q.val:
+                return False
+            return Same(p.left ,  q.left ) and Same(p.right , q.right)
+        return Same(p,q)
+
+# Time Complexity - O(p + q)
+# Space Complexity- O(h_p + h_q)
   
