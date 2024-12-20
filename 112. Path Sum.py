bf9b0ce66a6b6de19e35509ea4ca8363f633a92c
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-12-20 23:14:16 +0530

    Update 112. Path Sum.py

--- 112. Path Sum.py
+++ 112. Path Sum.py
@@ -1,1 +1,13 @@
+class Solution:
+  def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+    def has_sum(root,curr_sum):
+      if not root:
+        return False 
+      curr_sum += root.val
+      if not root.right and not root.left:
+        return root.val == targetSum
+      return has_sum(root.right,curr_sum) or has_sum(root.left, curr_sum)
+    return has_sum(root,0)
 
+# Time Complexity - O(n) , n  - no of nodes in tree 
+# Space Complexity - O(h) , h - height of tree 
