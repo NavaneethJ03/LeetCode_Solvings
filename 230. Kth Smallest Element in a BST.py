0fd0c4ab069355e60cbd61db1e1fa4fa14c3b8f2
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2024-12-29 23:50:20 +0530

    Update 230. Kth Smallest Element in a BST.py

--- 230. Kth Smallest Element in a BST.py
+++ 230. Kth Smallest Element in a BST.py
@@ -1,1 +1,22 @@
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+      count = [0]
+      ans =  [0]
+      def dfs(node):
+        if not node:
+          return None 
+        dfs(node.left)
+        if count[0] == 1:
+          ans[0] = node.val 
+        count[0] -= 1 
 
+        if count[0] > 0:
+          dfs(node.right)
+
+      dfs(root)
+      return ans[0]
+
+# Time Complexity -  O(N)
+# Space Complexity - O(h) 
+
+          
