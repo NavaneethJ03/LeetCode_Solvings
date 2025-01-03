14da96aabda079843370bcf422fd26530516f46a
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2025-01-03 12:22:30 +0530

    Update 2270. Number of Ways to Split Array.py

--- 2270. Number of Ways to Split Array.py
+++ 2270. Number of Ways to Split Array.py
@@ -1,1 +1,14 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+      count = 0 
+      leftsum = 0 
+      rightsum =  sum(nums)
+      for i in range(len(nums) - 1):
+        leftsum += nums[i]
+        rightsum -= nums[i] 
+        if leftsum >= rightsum:
+          count += 1 
+      return count
 
+# Time Complexity = O(n)
+# Space Complexity - O(1)
